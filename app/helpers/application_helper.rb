07e7b4fc52e4d5255bf4465737c24244bd4d5fa1
ogobet <trmokwena@gmail.com>
2015-08-27 23:55:57 +0200

    removing account controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,6 @@ module ApplicationHelper
   def gravatar_for(user, size = 30, title = user.name)
     image_tag gravatar_image_url(user.email, size: size), title: title, class: 'img-rounded'
   end
+
+  end
 end
