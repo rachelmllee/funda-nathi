694609b72a06ad9d5135b0ce7c24b1103bb06be1
mkwteb005 <mkwteb005@myuct.ac.za>
2015-09-11 20:59:36 +0000

    Play around with buttons

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,6 +5,7 @@ class AccountsController < ApplicationController
 
 	def show
 		@account = current_user.account
+		@id = current_user.user_id
 		unless @account
 			@account = Account.create(user_id: current_user.id, credit: 0, subcription: 0)			
 		end
